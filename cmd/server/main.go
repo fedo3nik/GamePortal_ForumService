d6cc52c8c82c74209f1c667e087c4362405dd5a5
fedo3nik <dimdomfedorov@gmail.com>
2021-03-22 15:29:58 +0300

    Add grpc connection

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,8 +5,11 @@ import (
 	"log"
 	"net/http"
 
+	"google.golang.org/grpc"
+
 	"github.com/fedo3nik/GamePortal_ForumService/internal/application/service"
 	"github.com/fedo3nik/GamePortal_ForumService/internal/config"
+	grpcInfra "github.com/fedo3nik/GamePortal_ForumService/internal/infrastructure/grpc"
 	"github.com/fedo3nik/GamePortal_ForumService/internal/interface/controller"
 	"github.com/gorilla/mux"
 	"github.com/jackc/pgx/v4/pgxpool"
@@ -25,17 +28,35 @@ func main() {
 
 	defer pool.Close()
 
+	grpcConn, err := grpc.Dial(c.GrpcPort, grpc.WithInsecure())
+	if err != nil {
+		log.Panicf("Grpc connection error: %v", err)
+	}
+
+	grpcClient := grpcInfra.NewSenderClient(grpcConn)
+
+	emp := grpcInfra.Empty{}
+
+	grpcResp, err := grpcClient.Send(context.Background(), &emp)
+	if err != nil {
+		log.Panicf("Grpc received error: %v", err)
+	}
+
 	handler := mux.NewRouter()
 
-	forumService := service.NewForumService(pool, "aKey", "rKey")
+	forumService := service.NewForumService(pool, grpcResp.AccessPublicKey, grpcResp.RefreshPublicKey)
 	addForumHandler := controller.NewHTTPAddForumHandler(forumService)
 	getForumHandler := controller.NewHTTPGetForumHandler(forumService)
 
 	handler.Handle("/forum/new-forum", addForumHandler).Methods("POST")
 	handler.Handle("/forum/get-forum/{id}", getForumHandler).Methods("GET")
 
-	err = http.ListenAndServe(c.Host+c.Port, handler)
-	if err != nil {
-		log.Panicf("Listen & Serve serror: %v", err)
-	}
+	go func() {
+		err = http.ListenAndServe(c.Host+c.Port, handler)
+		if err != nil {
+			log.Panicf("Listen & Serve serror: %v", err)
+		}
+	}()
+
+	select {}
 }
